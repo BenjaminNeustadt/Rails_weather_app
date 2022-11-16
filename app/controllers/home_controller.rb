353246b5c667e476627b6c2489d88347e3bdf12b
benjamin <b.james.neustadt@gmail.com>
2022-11-16 20:37:56 +0100

    It prints the status of the weather

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
+require 'net/http'
 
 class HomeController < ApplicationController
   def index
+    url = "https://api.openweathermap.org/data/2.5/weather?lat=25.04776&lon=121.53185&units=metric&appid=cda8498c63a04d3ca45e4797a9419edb"
+    uri = URI(url)
+    res = Net::HTTP.get_response(uri)
+    @data = JSON.parse(res.body)
   end
 end
 
