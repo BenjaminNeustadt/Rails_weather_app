85332722242957e2ccb4c1fb01f92d7e6372d04d
benjamin <b.james.neustadt@gmail.com>
2022-11-17 09:19:55 +0100

    Refactor logic from controller to services

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,7 @@
 # frozen_string_literal: true
 class HomeController < ApplicationController
   def index
-    url = "https://api.openweathermap.org/data/2.5/weather?lat=25.04776&lon=121.53185&units=metric&appid=cda8498c63a04d3ca45e4797a9419edb"
-    uri = URI(url)
-    res = Net::HTTP.get_response(uri)
-    @data = JSON.parse(res.body)
+    @data = CurrentWeatherService.new(latitude: "25.04776",longtitude:"121.53185", units:"metric").call
   end
 end
 
---
+++ app/services/current_weather_service.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+class CurrentWeatherService
+  BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
+
+  def initialize(latitude:, longtitude:, units: "metric")
+    @latitude = latitude
+    @longtitude = longtitude
+    @units = units
+  end
+
+  def call
+    response = Net::HTTP.get_response(uri)
+    JSON.parse(response.body)
+  end
+
+  private 
+
+  attr_reader :latitude, :longtitude, :units
+
+  def uri
+    return @uri if defined?(@uri)
+
+    @uri = URI(BASE_URL)
+    params = { lat: latitude, lon: longtitude, units: units, appid:"cda8498c63a04d3ca45e4797a9419edb" }
+    @uri.query = URI.encode_www_form(params)
+    @uri
+  end
+end
