effa048667513cb46d2ac6e917cd14049b72a66c
benjamin <b.james.neustadt@gmail.com>
2022-11-23 13:48:09 +0100

    Fix bug, default location is set to Alaska

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,6 +7,10 @@ class HomeController < ApplicationController
 
     @user_input = params[:q]
 
+    if @user_input.nil?
+      @user_input = "Alaska"
+    end
+
     # request_station = "Alaska"
     url = "https://api.openweathermap.org/geo/1.0/direct?q=#{@user_input}&appid=cda8498c63a04d3ca45e4797a9419edb"
 
